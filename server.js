60e9fea308b2b24917c1bc423a8213f8182be84d
yuttana76@gmail.com <yuttana@merchantasset.co.th>
2022-10-26 13:20:48 +0700

    Update Api route

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send('<h1>Express Demo App</h1> <h4>Message: Success</h4> <p>Version 1.1</p>');
 })
 
-app.use("/api/todo",todoRoute);
+// app.use("/api/todo",todoRoute);
 
 
 app.listen(3000, () => {
