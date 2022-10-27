f00192b3d0ea39fb29ff249b507bd87256cc0306
yuttana76@gmail.com <yuttana@merchantasset.co.th>
2022-10-27 15:52:45 +0700

    Update script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-#give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+echo 'run application_start.sh: ' >> /home/ec2-user/todos/deploy.log
 
-#navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
-
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
-#install node modules
-npm install
-
-#start our node app in the background
-node server.js > app.out.log 2> app.err.log < /dev/null & 
+echo 'pm2 restart nodejs-express-app' >> /home/ec2-user/todos/deploy.log
+pm2 restart nodejs-express-app >> /home/ec2-user/todos/deploy.log
